a0bd3672adefdac6021d049d8f4bd694f86d5e22
pablo <sadowizzy@gmail.com>
2022-07-19 23:56:24 +0200

    created and added nested/child component in the product-list container using it selector

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ProductListComponent } from './products/product.list.component';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { convertToSpacePipe } from './products/convert-to-spaces.pipe';
+import { convertToSpacePipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    convertToSpacePipe
+    convertToSpacePipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'pm-star',
+    templateUrl:'./star.component.html',
+    styleUrls:['./star.component.css']
+})
+export class StarComponent{
+    rating: number = 4;
+    cropWidth: number = 75;
+
+}
