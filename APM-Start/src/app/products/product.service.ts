1b1ebec4e82baeda9a5240c2dc315c70f75e9efc
pablo <sadowizzy@gmail.com>
2022-08-06 20:56:11 +0200

    http error handling

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,18 +1,31 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient,HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { catchError, Observable,tap, throwError } from "rxjs";
 import { IProduct } from "./products";
 
 @Injectable(
 )  
 export class ProductService{
- 
-  private productUrl='www.myWebService.com/api/products';
-
-  constructor(private http:HttpClient){
-
-    }
+  //whoever we need to define the location of the file so that angular knows when it startup the application 
+  private productUrl='api/products/products.json';
+  constructor(private http:HttpClient){}    
    public getProducts():Observable<IProduct[]>{ // the method does nothing untill we subscribe
-      return this.http.get<IProduct[]>(this.productUrl);//get<IProduct[]> types/associates/maps the response to an array of product
+      return this.http.get<IProduct[]>(this.productUrl).pipe(
+        tap(data => console.log('all:',JSON.stringify(data))),
+        catchError(this.handleError)
+      );
    }
+   private handleError(err:HttpErrorResponse){
+      
+    let errorMessage='';
+    if(err.error instanceof ErrorEvent){
+    errorMessage=`An error occured: ${err.error.message}`
+    }else{
+      errorMessage= `Server returned code: ${err.status}, error message is ${err.message}`
+    }
+
+    // return throwError(()=>new Error('something bad happen in the backend'));  
+      return throwError(()=>errorMessage);
+   }
+   
 }
