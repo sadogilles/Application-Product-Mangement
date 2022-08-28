9da04529be27cbeeba8c1cb63f3f81a9fdf4c084
pablo <sadowizzy@gmail.com>
2022-08-28 12:31:08 +0200

    defining routes in app.module.ts

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -6,18 +6,33 @@ import { FormsModule } from '@angular/forms';
 import { convertToSpacePipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     convertToSpacePipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    //order masters -always the most specific at the top and least at the button
+    //wildcard should always be at the bottom at it matches route not specified in the array
+    //note that the path doesnot start with a /
+    RouterModule.forRoot([
+      {path:'products',component:ProductListComponent},
+      {path:'products:id',component:ProductDetailComponent},
+      {path:'welcome',component:WelcomeComponent},
+      {path:'',redirectTo:'welcome',pathMatch:'full'},
+      {path:'**',redirectTo:'welcome',pathMatch:'full'}
+
+    ])
   ],
   bootstrap: [AppComponent]
 })
